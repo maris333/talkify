8ac873c72746295e29e66b99b8e11d0d210466b4
maris333 <animaral@gmail.com>
2023-11-05 14:50:29 +0100

    Add translation

--- main.py
+++ main.py
@@ -1,39 +1,8 @@
-from flask import Flask, render_template, request
-import boto3
-from decouple import config
+from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        text_to_convert = request.form['text_to_convert']
-
-        aws_access_key_id = config('aws_access_key_id')
-        aws_secret_access_key = config('aws_secret_access_key')
-        aws_region = config('aws_region')
-        s3_bucket_name = config('s3_bucket_name')
-
-        polly_client = boto3.client('polly', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=aws_region)
-        s3_client = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=aws_region)
-
-        response = polly_client.synthesize_speech(
-            Text=text_to_convert,
-            OutputFormat='mp3',
-            VoiceId='Joanna'
-        )
-
-        output_file = 'output.mp3'
-        with open(output_file, 'wb') as f:
-            f.write(response['AudioStream'].read())
-
-        s3_client.upload_file(output_file, s3_bucket_name, output_file)
-
-        return f'MP3 file was converted and saved: s3://{s3_bucket_name}/{output_file}'
-
-    return render_template('index.html')
-
-
 if __name__ == '__main__':
+    from views import index_blueprint
+    app.register_blueprint(index_blueprint)
     app.run(debug=True)
---
+++ views.py
@@ -0,0 +1,25 @@
+from flask import request, render_template, Blueprint
+
+from aws_manager import s3_bucket_name, upload_file, save_file, translate_text, get_translated_text
+
+index_blueprint = Blueprint("index", __name__)
+
+
+@index_blueprint.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        text_to_convert = request.form['text_to_convert']
+        source_language = request.form['source_language']
+        target_language = request.form['target_language']
+
+        translated_text = translate_text(text_to_convert, source_language, target_language)
+
+        response, s3_client = get_translated_text(translated_text)
+
+        output_file = save_file(response)
+
+        upload_file(output_file, s3_client)
+
+        return f'MP3 file was converted and saved: s3://{s3_bucket_name}/{output_file}'
+
+    return render_template('index.html')
